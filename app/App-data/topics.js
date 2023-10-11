70b432b6bddc817eec20561072b2cd48b018f114
vicxtunespro <vicitug@gmail.com>
2023-10-11 04:40:19 -0700

    Add a new folder form my data.

---
+++ app/App-data/topics.js
@@ -0,0 +1,19 @@
+const topics = [
+    {
+        topic: "Memory Management",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+        image:"memory.jpg"
+    },
+    {
+        topic: "Process Management",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+        image:"process.jpg"
+    },
+    {
+        topic: "File Management",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+        image:"file.jpg"
+    },
+]
+
+export default topics
--- app/Components/package.jsx
+++
@@ -1,7 +0,0 @@
-export function Package() {
-    return(
-        <div className="price-card">
-          
-        </div>
-    );
-}
